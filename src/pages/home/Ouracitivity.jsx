ed98d018bde34ca3f9d3a3d157e79f87762b4236
daksh888 <godhanidaksh016@gmail.com>
2024-01-26 17:18:16 +0530

    commit

--- src/pages/home/Ouracitivity.jsx
+++ src/pages/home/Ouracitivity.jsx
@@ -8,44 +8,42 @@ const Ouractivity = () => {
         <Card style={{ width: "18rem" }}>
           <Card.Img className="ouractivity-img" variant="top" src="/a.png" />
           <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
+            <Card.Title>Services performed in the institution</Card.Title>
+            {/* <Card.Text>
+            To bring unclaimed people to the organization who are roaming on the road.
+            </Card.Text> */}
             
           </Card.Body>
         </Card>
         <Card style={{ width: "18rem"}}>
-          <Card.Img className="ouractivity-img" variant="top" src="/b.png" />
+          <Card.Img className="ouractivity-img" variant="top" src="/mahanubha2.png" />
           <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
+            <Card.Title>Political dignitaries visit the institute</Card.Title>
+            {/* <Card.Text>
+            Honorable Home Minister of India Mr. Amitbhai Shah and President of the Institute Mr. Dineshbhai Lathiya had a congratulatory visit.
+            </Card.Text> */}
             
           </Card.Body>
         </Card>
         <Card style={{ width: "18rem" }}>
-          <Card.Img className="ouractivity-img" variant="top" src="/c.png" />
+          <Card.Img className="ouractivity-img" variant="top" src="/seva4.png" />
           <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
+            <Card.Title>Reunion with family through institution</Card.Title>
+            {/* <Card.Text>
               Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
+              
+            </Card.Text> */}
             
           </Card.Body>
         </Card>
         <Card style={{ width: "18rem" }}>
-          <Card.Img className="ouractivity-img" variant="top" src="/d.png" />
+          <Card.Img className="ouractivity-img" variant="top" src="/aabhar.png" />
           <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
+            <Card.Title>An expression of thanks</Card.Title>
+            {/* <Card.Text>
               Some quick example text to build on the card title and make up the
               bulk of the card's content.
-            </Card.Text>
+            </Card.Text> */}
             
           </Card.Body>
         </Card>
